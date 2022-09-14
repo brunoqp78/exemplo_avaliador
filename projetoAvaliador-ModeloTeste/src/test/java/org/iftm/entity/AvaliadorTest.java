e3859dbc40f76f451603f04a838c9a870bd7ef79
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2022-09-14 14:55:35 -0300

    Exemplo avaliador parte 2

--- projetoAvaliador-ModeloTeste/src/test/java/org/iftm/entity/AvaliadorTest.java
+++ projetoAvaliador-ModeloTeste/src/test/java/org/iftm/entity/AvaliadorTest.java
@@ -1,10 +1,13 @@
 package org.iftm.entity;
 
 import org.iftm.entities.Avaliador;
+import org.iftm.entities.Lance;
 import org.iftm.entities.Leilao;
 import org.iftm.entities.Usuario;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AvaliadorTest {
 	private Usuario usuario1;
@@ -23,9 +26,91 @@ public class AvaliadorTest {
 		leilao = new Leilao("Computador Apple");
 		avaliador = new Avaliador();
 	}
+
+	@Test
+	public void testarBuscaMaiorLanceSemOrdem() {
+		// Entradas : criar os lances e o resultado esperado
+		Lance lance1 = new Lance(usuario1, 300);
+		Lance lance2 = new Lance(usuario2, 400.0000001);
+		Lance lance3 = new Lance(usuario3, 250);
+		leilao.propoe(lance1);
+		leilao.propoe(lance2);
+		leilao.propoe(lance3);
+		
+		double resultadoEsperado = 400;
+
+		// execução
+		avaliador.avalia(leilao);
+		double resultadoObtido = avaliador.getMaiorLance();
+
+		// comparação
+		// Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+
+	}
+
+	@Test
+	public void testarBuscaMaiorLanceComOrdem() {
+//Entradas : criar os lances e o resultado esperado
+		Lance lance1 = new Lance(usuario1, 300);
+		Lance lance2 = new Lance(usuario2, 400.0000001);
+		Lance lance3 = new Lance(usuario3, 250);
+		leilao.propoe(lance3);
+		leilao.propoe(lance1);
+		leilao.propoe(lance2);
+		
+		double resultadoEsperado = 400;
+
+//execução 
+		avaliador.avalia(leilao);
+		double resultadoObtido = avaliador.getMaiorLance();
+
+//comparação
+		// assertEquals(resultadoEsperado, resultadoObtido);
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+	}
 	
 	@Test
-	public void meuPrimeiroTeste() {
+	public void testarBuscaMenorLanceSemOrdem() {
+		// Entradas : criar os lances e o resultado esperado
+		Lance lance1 = new Lance(usuario1, 300);
+		Lance lance2 = new Lance(usuario2, 400.0000001);
+		Lance lance3 = new Lance(usuario3, 250);
+		leilao.propoe(lance1);
+		leilao.propoe(lance2);
+		leilao.propoe(lance3);
 		
+		double resultadoEsperado = 250;
+
+		// execução
+		avaliador.avalia(leilao);
+		double resultadoObtido = avaliador.getMenorLance();
+
+		// comparação
+		// Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+
 	}
+
+	@Test
+	public void testarBuscaMenorLanceComOrdem() {
+//Entradas : criar os lances e o resultado esperado
+		Lance lance1 = new Lance(usuario1, 300);
+		Lance lance2 = new Lance(usuario2, 400.0000001);
+		Lance lance3 = new Lance(usuario3, 250);
+		leilao.propoe(lance3);
+		leilao.propoe(lance1);
+		leilao.propoe(lance2);
+		
+		double resultadoEsperado = 250;
+
+//execução 
+		avaliador.avalia(leilao);
+		double resultadoObtido = avaliador.getMenorLance();
+
+//comparação
+		// assertEquals(resultadoEsperado, resultadoObtido);
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+	}	
+
 }
